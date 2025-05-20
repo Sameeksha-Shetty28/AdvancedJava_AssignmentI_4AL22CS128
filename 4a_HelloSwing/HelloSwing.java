5803bc2b9429e5da37eee8ec06820840207e7708
Sameeksha-Shetty28 <shettysameeksha066@gmail.com>
2025-05-20 11:47:21 +0530

    Add files via upload

---
+++ 4a_HelloSwing/HelloSwing.java
@@ -0,0 +1,21 @@
+package java_question_4a;
+import javax.swing.*;
+import java.awt.*;
+
+public class HelloSwing {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("HelloSwing Program");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(700, 200);
+        frame.setLocationRelativeTo(null);
+
+        JLabel label = new JLabel("Hello! VI C, Welcome to Swing Programming");
+        label.setForeground(Color.BLUE);
+        label.setFont(new Font("Serif", Font.PLAIN, 32));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+
+        frame.add(label);
+        frame.setVisible(true);
+    }
+}
+
